02fda86f813287a6aa01c0c6a733f575c8fe9e6c
Luke Berry <lukeberrypi@gmail.com>
2023-12-22 13:26:43 -0300

    refactor

--- 2023/7/a.js
+++ 2023/7/a.js
@@ -1,11 +1,35 @@
-const file = Bun.file("./2023/3/input.txt");
+const file = Bun.file("./2023/7/input.txt");
 const input = await file.text();
 
 const lines = input.split("\n");
 
-function gearRatios(lines) {
-  const matrix = lines.map((str) => str.split(""));
-  return matrix;
+function cardBidMap(lines) {
+  const dict = lines.reduce((acc, line) => {
+    const [card, bid] = line.split(" ");
+    acc[card] = bid;
+    return acc;
+  }, {});
+
+  return dict;
 }
 
-console.log("gearRatios", gearRatios(lines));
+function cardRankMap(cardBidMap) {
+  let cardRankDict = new Map();
+  let other = Object.keys(cardBidMap).map((key) => {
+    const inside = key.split("").map((char) => {
+      if (cardRankDict.has(char)) return;
+      let count = key.match(new RegExp(char, "g") || []).length;
+      return cardRankDict[char] = count;
+    }).filter(Boolean);
+
+    return inside;
+  })
+
+  return other;
+}
+
+function main() {
+  return undefined;
+}
+
+console.log("cardRankMap(lines)", cardRankMap(cardBidMap(lines)));
