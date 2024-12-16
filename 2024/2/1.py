7c88d75b70628cf00771ea702a202df11f7a1dfc
Luke Berry <lukeberrypi@gmail.com>
2024-12-16 23:44:53 +0000

    details

--- 2024/2/1.py
+++ 2024/2/1.py
@@ -5,12 +5,24 @@ def parse(s: str) -> List[List[int]]:
     to_lists = list(map(lambda line: line.split(" "), spl))
     return [[int(x) for x in xs] for xs in to_lists]
 
-def main(s):
+def main(s) -> int:
     lsts = parse(s)
+    safe_lists = 0
     for lst in lsts:
-        pass
-
-    return 0
+        diffs = []
+        is_safe = True
+        for left, right in zip(lst, lst[1:]):
+            diff = right - left
+            if abs(diff) > 3 or abs(diff) < 1:
+                is_safe = False
+                break
+            diffs.append(diff > 0)
+        if is_safe:
+            ascending = all(diffs)
+            descending = not any(diffs)
+            if ascending or descending:
+                safe_lists += 1
+    return safe_lists
 
 with open('2024/2/input.txt') as f:
     print(main(f.read()))
