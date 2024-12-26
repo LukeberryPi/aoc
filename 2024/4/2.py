87e249df17b84eb1b5777b8ae983af8a93311216
Luke Berry <lukeberrypi@gmail.com>
2024-12-26 19:23:36 +0000

    day4pt2

---
+++ 2024/4/2.py
@@ -0,0 +1,37 @@
+from typing import Tuple, List
+import re
+
+def parse(s: str) -> List[List[str]]:
+    return [list(x) for x in s.split("\n")]
+
+# for a given A, check for 2 S and 2 M diagonally adjacent
+# same letter cannot be in the same diagonal
+
+def find_letter_coordinates(matrix: List[List[int]], letter: str) -> List[Tuple[int, int]]:
+    pos = []
+    for i, lst in enumerate(matrix):
+        for j in range(len(lst)):
+            if matrix[i][j] == letter:
+                pos.append((i, j))
+    return pos
+
+def main(s: str) -> int:
+    count = 0
+    matrix = parse(s)
+    dic = {}
+    for ch in "MAS":
+        dic[ch] = find_letter_coordinates(matrix, ch)
+    for a in dic["A"]:
+        a_row, a_col = a
+        s_count, m_count = 0, 0
+        for dx, dy in [(1, 1), (-1, 1), (-1, -1), (1, -1)]:
+            if (a_row + dx, a_col + dy) in dic["M"] and (a_row - dx, a_col - dy) in dic["M"]: continue
+            if (a_row + dx, a_col + dy) in dic["S"] and (a_row - dx, a_col - dy) in dic["S"]: continue
+            s_count += 1 if (a_row + dx, a_col + dy) in dic["S"] else 0
+            m_count += 1 if (a_row + dx, a_col + dy) in dic["M"] else 0
+        if s_count == 2 and m_count == 2:
+            count += 1
+    return count
+
+with open("2024/4/input.txt") as f:
+    print(main(f.read()))
