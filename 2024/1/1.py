66ea2b542968db00bf519e152c1cd4e609206c8a
Luke Berry <lukeberrypi@gmail.com>
2024-12-30 10:40:54 +0000

    types

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple
 
-def input_to_lists(s: str) -> Tuple[List[int]]:
+def input_to_lists(s: str) -> Tuple[List[int], List[int]]:
     to_lines = list(map(lambda line: line.split("   "), s.split("\n")))
     flatten = [int(x) for xs in to_lines for x in xs]
     left, right = flatten[::2], flatten[1::2]
@@ -11,7 +11,6 @@ def main(s: str) -> int:
     sorted_left, sorted_right = sorted(input_to_lists(s)[0]), sorted(input_to_lists(s)[1])
     for i in range(len(sorted_left)):
         res += abs(sorted_left[i] - sorted_right[i])
-
     return res
 
 with open('2024/1/input.txt') as f:
