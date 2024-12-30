6993ab8a21135edc7fb92be3bdcca53274a040e2
Luke Berry <lukeberrypi@gmail.com>
2024-12-30 13:42:20 +0000

    day5pt1

---
+++ 2024/5/1.py
@@ -0,0 +1,30 @@
+from typing import List, Tuple
+
+def parse_rules(s: str) -> List[Tuple[str, ...]]:
+    spl = s.split("\n\n")[0]
+    return [tuple(rule.split("|")) for rule in spl.split("\n")]
+
+def parse_updates(s: str) -> List[List[str]]:
+    spl = s.split("\n\n")[1]
+    return [update.split(",") for update in spl.split("\n")]
+
+def main(s: str) -> int:
+    res = 0
+    rules = parse_rules(s)
+    updates = parse_updates(s)
+    for update in updates:
+        valid = True
+        for i, upd in enumerate(update):
+            befores = update[:i]
+            afters = update[i + 1:]
+            relevant_rules = [rule for rule in rules if upd in rule]
+            is_invalid = any(rule[0] in afters or rule[1] in befores for rule in relevant_rules)
+            if is_invalid:
+                valid = False
+        if valid:
+            res += int(update[len(update) // 2])
+    return res
+
+
+with open("2024/5/input.txt") as f:
+    print(main(f.read()))
