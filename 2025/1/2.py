6763cbe7e2a8053d9751974fac5ad3782dc5b6bd
Luke Berry <lukeberrypi@gmail.com>
2025-12-03 18:17:12 +0000

    details

--- 2025/1/2.py
+++ 2025/1/2.py
@@ -11,29 +11,29 @@ def main(s: str) -> int:
     current = 50
     hits0 = 0;
     for (direction, amount) in tuples:
+        print("current starts at", current)
         amount = int(amount)
-
-        print("old", current)
-        print(direction, amount)
-
+        quotient, remainder = divmod(amount, 100)
+        if current == 0:
+            hits0 += 1
         if direction == "R":
-            current += amount
-            if current > 99 and current != 100:
-                hits0 += 1
+            current += remainder
+            if current > 99:
                 current -= 100
-        if direction == "L":
-            current -= amount
-            if current < 0:
                 hits0 += 1
+        if direction == "L":
+            current -= remainder
+            if current < 0:
                 current += 100
-            
-        print("new", current)
-        print("")
-        print("hits", hits0)
-        print("\n")
-
+                hits0 += 1
+        if current == 100:
+            current = 0
+        print(f"moves {direction} for {amount}")
+        hits0 += quotient
+        print("current ends at", current)
+        print(f"0 has been hit {hits0} times\n")
     return hits0
         
 
-with open('2025/1/input.txt') as inpt:
+with open('2025/1/sample.txt') as inpt:
     print("result", main(inpt.read()))
