86f7f29c31eb140f8d48bf5eb2694d2d52f71b4f
Luke Berry <lukeberrypi@gmail.com>
2025-12-24 16:26:30 +0000

    day5COMPLETE

--- 2025/5/2.py
+++ 2025/5/2.py
@@ -7,13 +7,23 @@ def parse(s: str):
     return ranges
 
 def main(s: str):
-    fresh = set()
     ranges = parse(s)
+    result = 0
+    tuples = []
     for rang in ranges:
-        bot, top = [int(n) for n in rang.split("-")]
-        for n in range(bot, top + 1):
-            fresh.add(n)
-    return len(fresh)
+        start, end = [int(n) for n in rang.split("-")]
+        tuples.append((start, end))
+    tuples.sort()
+    cur_start, cur_end = tuples[0]
+    for tup in tuples[1:]:
+        start, end = tup
+        if start <= cur_end:
+            cur_end = max(cur_end, end)
+        else:
+            result += cur_end - cur_start + 1
+            cur_start, cur_end = start, end
+    result += cur_end - cur_start + 1 
+    return result
             
 
 with open('2025/5/input.txt') as f:
